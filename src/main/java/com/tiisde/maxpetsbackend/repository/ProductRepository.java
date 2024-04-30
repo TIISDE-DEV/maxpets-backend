c0b157118ebc1afd064b2338e7bcb413ddec37c3
andre.borba <dev_andre.filipe@outlook.com>
2024-04-30 16:57:16 -0300

    refactor: moved to dataprovider

--- src/main/java/com/tiisde/maxpetsbackend/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tiisde.maxpetsbackend.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.tiisde.maxpetsbackend.core.domain.Product;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-
-}
