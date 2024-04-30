bf7ca2f79d3369c744004b2fae6f452c353869b6
andre.borba <dev_andre.filipe@outlook.com>
2024-04-30 16:56:17 -0300

    styles:

--- src/main/java/com/tiisde/maxpetsbackend/config/seeds/ProductSeed.java
+++ src/main/java/com/tiisde/maxpetsbackend/config/seeds/ProductSeed.java
@@ -2,8 +2,8 @@ package com.tiisde.maxpetsbackend.config.seeds;
 
 import com.github.javafaker.Faker;
 import com.tiisde.maxpetsbackend.core.domain.Product;
-import com.tiisde.maxpetsbackend.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.tiisde.maxpetsbackend.dataprovider.repository.ProductRepository;
+
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -16,11 +16,10 @@ public class ProductSeed {
     private final ProductRepository productRepository;
     private final Faker faker;
 
-    private final String[] availableSizes = {"S", "M", "L", "XL", "XXL"};
-    private final String[] availableColors = {"Blue", "Green", "Red"};
-    private final Byte[] images = {null};
+    private final String[] availableSizes = { "S", "M", "L", "XL", "XXL" };
+    private final String[] availableColors = { "Blue", "Green", "Red" };
+    private final Byte[] images = { null };
 
-    @Autowired
     public ProductSeed(ProductRepository productRepository, Faker faker) {
         this.productRepository = productRepository;
         this.faker = faker;
@@ -29,15 +28,16 @@ public class ProductSeed {
     @PostConstruct
     public void seed() {
         List<Product> products = new ArrayList<>();
-        for (int i = 0; i < 10; i++) { 
-            Product product = new Product(null, null, i, null, availableColors, i, images, availableColors);
+        for (int i = 0; i < 10; i++) {
+            Product product = new Product(null, null, i, null, availableColors, i,
+                    images, availableColors);
             product.setName(faker.commerce().productName());
             product.setPrice(Double.parseDouble(faker.commerce().price()));
             product.setSize(faker.options().option(availableSizes));
-            product.setAvailableSizes(availableSizes); 
+            product.setAvailableSizes(availableSizes);
             product.setTotalReviews(faker.number().numberBetween(0, 1000));
-            product.setImages(images); 
-            product.setAvailableColors(availableColors); 
+            product.setImages(images);
+            product.setAvailableColors(availableColors);
             products.add(product);
         }
         productRepository.saveAll(products);
