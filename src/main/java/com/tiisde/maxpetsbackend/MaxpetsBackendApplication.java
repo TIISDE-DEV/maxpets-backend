cec56d3783fc7ffb4833912e714315837b77c066
andre.borba <dev_andre.filipe@outlook.com>
2024-04-30 13:15:42 -0300

    feat: initial commit

---
+++ src/main/java/com/tiisde/maxpetsbackend/MaxpetsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tiisde.maxpetsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxpetsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaxpetsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiisde/maxpetsbackend/MaxpetsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiisde.maxpetsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxpetsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
