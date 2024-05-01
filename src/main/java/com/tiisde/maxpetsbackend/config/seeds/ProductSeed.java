5b903c91568d89287317d4fe9540d79afaedd769
alexsouza <alexsouza160298@gmail.com>
2024-05-01 08:41:04 -0300

    feat: product-seed

--- src/main/java/com/tiisde/maxpetsbackend/config/seeds/ProductSeed.java
+++ src/main/java/com/tiisde/maxpetsbackend/config/seeds/ProductSeed.java
@@ -2,13 +2,17 @@ package com.tiisde.maxpetsbackend.config.seeds;
 
 import com.github.javafaker.Faker;
 import com.tiisde.maxpetsbackend.core.domain.Product;
+import com.tiisde.maxpetsbackend.dataprovider.entity.ProductEntity;
 import com.tiisde.maxpetsbackend.dataprovider.repository.ProductRepository;
 
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class ProductSeed {
@@ -20,6 +24,11 @@ public class ProductSeed {
     private final String[] availableColors = { "Blue", "Green", "Red" };
     private final Byte[] images = { null };
 
+    private BigDecimal convertToBigDecimal(String priceString) {
+        String normalizedPriceString = priceString.replace(",", ".");
+        return new BigDecimal(normalizedPriceString);
+    }
+
     public ProductSeed(ProductRepository productRepository, Faker faker) {
         this.productRepository = productRepository;
         this.faker = faker;
@@ -29,17 +38,33 @@ public class ProductSeed {
     public void seed() {
         List<Product> products = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
-            Product product = new Product(null, null, i, null, availableColors, i,
-                    images, availableColors);
-            product.setName(faker.commerce().productName());
-            product.setPrice(Double.parseDouble(faker.commerce().price()));
-            product.setSize(faker.options().option(availableSizes));
-            product.setAvailableSizes(availableSizes);
-            product.setTotalReviews(faker.number().numberBetween(0, 1000));
-            product.setImages(images);
-            product.setAvailableColors(availableColors);
+            Product product = new Product(
+                    String.valueOf(i),
+                    faker.commerce().productName(),
+                    convertToBigDecimal(faker.commerce().price()),
+                    faker.options().option(availableSizes),
+                    availableSizes,
+                    faker.number().numberBetween(0, 1000),
+                    images,
+                    availableColors);
             products.add(product);
         }
-        productRepository.saveAll(products);
+
+        List<ProductEntity> entities = products.stream()
+                .map(product -> {
+                    ProductEntity entity = new ProductEntity();
+                    entity.setId(product.getId());
+                    entity.setName(product.getName());
+                    entity.setPrice(product.getPrice());
+                    entity.setSize(product.getSize());
+                    entity.setAvailableSizes(product.getAvailableSizes());
+                    entity.setTotalReviews(product.getTotalReviews());
+                    entity.setImages(product.getImages());
+                    entity.setAvailableColors(product.getAvailableColors());
+                    return entity;
+                })
+                .collect(Collectors.toList());
+
+        productRepository.saveAll(entities);
     }
 }
