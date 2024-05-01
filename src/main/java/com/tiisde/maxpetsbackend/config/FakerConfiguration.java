136aa8f391a9cc6e159b4abd3cfc6b5c6019ee6c
alexsouza <alexsouza160298@gmail.com>
2024-05-01 08:33:12 -0300

    feat: faker-config

---
+++ src/main/java/com/tiisde/maxpetsbackend/config/FakerConfiguration.java
@@ -0,0 +1,15 @@
+package com.tiisde.maxpetsbackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.github.javafaker.Faker;
+
+@Configuration
+public class FakerConfiguration {
+    
+    @Bean
+    public Faker faker() {
+        return new Faker();
+    }
+}
